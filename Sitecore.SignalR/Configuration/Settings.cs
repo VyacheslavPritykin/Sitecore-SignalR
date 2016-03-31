c3beb1a77dd248f9722ee4c614ae2ef5e132d4f6
Vyacheslav Pritykin <pritykin.amir@gmail.com>
2016-03-31 05:13:12 +0300

    Initial solution commit

---
+++ Sitecore.SignalR/Configuration/Settings.cs
@@ -0,0 +1,11 @@
+﻿namespace Sitecore.SignalR.Configuration
+{
+  public static class Settings
+  {
+    public static class SignalR
+    {
+      public static string Path => Sitecore.Configuration.Settings.GetSetting("SignalR.Path", "/signalr");
+    }
+  }
+}
+
---
+++ Sitecore.SignalR/Pipelines/InitializeOwinMiddleware/InitializeSignalR.cs
@@ -0,0 +1,19 @@
+﻿using Owin;
+
+namespace Sitecore.SignalR.Pipelines.InitializeOwinMiddleware
+{
+  using Microsoft.AspNet.SignalR;
+
+  using Sitecore.Diagnostics;
+  using Sitecore.Owin.Pipelines.InitializeOwinMiddleware;
+  using Sitecore.SignalR.Configuration;
+
+  public class InitializeSignalR
+  {
+    public void Process([NotNull] InitializeOwinMiddlewareArgs args)
+    {
+      Assert.ArgumentNotNull(args, nameof(args));
+      args.App.MapSignalR(Settings.SignalR.Path, new HubConfiguration());
+    }
+  }
+}
