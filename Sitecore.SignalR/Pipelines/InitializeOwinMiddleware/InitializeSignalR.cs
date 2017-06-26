f438072099109d1e015278ca908f78b21c06145a
Vyacheslav Pritykin <vpr@sitecore.net>
2017-06-26 19:09:01 +0300

    Add support for Sitecore 9.x

--- Sitecore.SignalR/Pipelines/InitializeOwinMiddleware/InitializeSignalR.cs
+++ Sitecore.SignalR/Pipelines/InitializeOwinMiddleware/InitializeSignalR.cs
@@ -5,12 +5,12 @@ namespace Sitecore.SignalR.Pipelines.InitializeOwinMiddleware
   using Microsoft.AspNet.SignalR;
 
   using Sitecore.Diagnostics;
-  using Sitecore.Owin.Pipelines.InitializeOwinMiddleware;
+  using Sitecore.Owin.Pipelines.Initialize;
   using Sitecore.SignalR.Configuration;
 
-  public class InitializeSignalR
+  public class InitializeSignalR : InitializeProcessor
   {
-    public void Process([NotNull] InitializeOwinMiddlewareArgs args)
+    public override void Process([NotNull] InitializeArgs args)
     {
       Assert.ArgumentNotNull(args, nameof(args));
       args.App.MapSignalR(Settings.SignalR.Path, new HubConfiguration());
--- Sitecore.SignalR/Properties/AssemblyInfo.cs
+++ Sitecore.SignalR/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sitecore SignalR")]
-[assembly: AssemblyDescription("SignalR support for Sitecore")]
+[assembly: AssemblyDescription("SignalR support for Sitecore 9.x")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Sitecore.SignalR")]
-[assembly: AssemblyCopyright("Copyright ©  2016 Vyacheslav Pritykin")]
+[assembly: AssemblyCopyright("Copyright ©  2017 Vyacheslav Pritykin")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.*")]
-[assembly: AssemblyInformationalVersion("2.0.0")]
+[assembly: AssemblyVersion("3.0.0.*")]
+[assembly: AssemblyInformationalVersion("3.0.0")]
